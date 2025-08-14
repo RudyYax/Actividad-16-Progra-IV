a94875bf4d4954db4f67e7fec572b28a52094bc8
Rudy <rudyax2004@gmail.com>
2025-08-13 19:26:35 -0600

    segundo Commit

--- Actividad 16 progra IV.py
+++ Actividad 16 progra IV.py
@@ -1,1 +1,12 @@
-print("Bienvenidos  Actividad 16")
+class Biblioteca:
+    def __init__(self, titulo, autor, anio_publicacion, codigo, nombre, carnet, carrera):
+        self.titulo = titulo
+        self.autor = autor
+        self.anio_publicacion = anio_publicacion
+        self.codigo = codigo
+        self.nombre = nombre
+        self.carnet = carnet
+        self.carrera = carrera
+
+    def mostrar(self):
+        print(f"El libro es: {self.libro}, el usuario es: {self.usuario}")
